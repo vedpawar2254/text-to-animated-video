076526dfeeb846b680468fa6bd2e4d589d3c8093
ved pawar <85354558+vedpawar2254@users.noreply.github.com>
2025-05-15 04:20:25 +0530

    Update main.py

--- example/main.py
+++ example/main.py
@@ -11,4 +11,3 @@ class DefaultTemplate(Scene):
         self.play(Create(square))  # animate the creation of the square
         self.play(Transform(square, circle))  # interpolate the square into the circle
         self.play(FadeOut(square))  # fade out animation
-
